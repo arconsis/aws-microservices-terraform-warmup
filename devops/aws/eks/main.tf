a9bb70b1ca47e4ec50cee147583d539fb40807b5
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-07-21 08:26:15 +0300

    remove comments

--- devops/aws/eks/main.tf
+++ devops/aws/eks/main.tf
@@ -75,40 +75,9 @@ module "eks" {
   ]
 }
 
-
 resource "aws_iam_policy" "worker_policy" {
   name        = "worker-policy"
   description = "Worker policy for the ALB Ingress"
 
   policy = file("../common/templates/eks/iam-policy.json")
 }
-
-# provider "helm" {
-#   version = "1.3.1"
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#     load_config_file       = false
-#   }
-# }
-
-# resource "helm_release" "ingress" {
-#   name       = "ingress"
-#   chart      = "aws-alb-ingress-controller"
-#   # repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
-#   version    = "1.0.2"
-
-#   set {
-#     name  = "autoDiscoverAwsRegion"
-#     value = "true"
-#   }
-#   set {
-#     name  = "autoDiscoverAwsVpcID"
-#     value = "true"
-#   }
-#   set {
-#     name  = "clusterName"
-#     value = var.cluster_name
-#   }
-# }
