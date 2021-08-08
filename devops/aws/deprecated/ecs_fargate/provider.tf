07802e648f64fe1fdab6c97f40a2da018c8c3dee
Alex Koufatzis <akoufa@hotmail.com>
2021-08-08 15:58:09 +0300

    Remove deprecated setup

--- devops/aws/deprecated/ecs_fargate/provider.tf
+++
@@ -1,6 +0,0 @@
-# Specify the provider and access details
-provider "aws" {
-  shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "default"
-  region                  = var.aws_region
-}
