3b34fbc12aaa6158d50bf9f621c9fa259c604d28
Alex Koufatzis <akoufa@hotmail.com>
2021-08-11 20:47:26 +0300

    Fix deploy.sh. Upgrade quarkus to 2.1.2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-echo -n "Please enter a lauch type of ECS (ec2 OR fargate OR eks OR lambda): "
+echo "Please enter a lauch type of ECS (ec2 OR fargate OR eks OR lambda): "
 read lauch_type
 
 if [ $lauch_type != "ec2" ] && [ $lauch_type != "fargate" ] && [ $lauch_type != "eks" ] && [ $lauch_type != "lambda" ]; then
@@ -11,32 +11,32 @@ cd ./backend/services/booksService &&
 ./deploy.sh &&
 
 # Deploy users API docker image
-cd ../backend/usersService &&
+cd ../usersService &&
 ./deploy.sh &&
 
 # Deploy recommendations API docker image
-cd ../backend/recommendationsService &&
+cd ../recommendationsService &&
 ./deploy.sh &&
 
 # Deploy promotions API docker image
-cd ../backend/promotionsService &&
+cd ../promotionsService &&
 ./deploy.sh &&
 
 # Deploy to cloud host (default AWS)
 if [ $lauch_type = "fargate" ]; then
-  cd ../../devops/aws/ecs_fargate &&
+  cd ../../../devops/aws/ecs_fargate &&
   terraform init
   terraform apply
 elif [ $lauch_type = "ec2" ]; then
-  cd ../../devops/aws/ecs_ec2 &&
+  cd ../../../devops/aws/ecs_ec2 &&
   terraform init
   terraform apply
 elif [ $lauch_type = "eks" ]; then
-  cd ../../devops/aws/eks &&
+  cd ../../../devops/aws/eks &&
   terraform init
   terraform apply
 else
-  cd ../../devops/aws/serverless &&
+  cd ../../../devops/aws/serverless &&
   terraform init
   terraform apply
 fi
