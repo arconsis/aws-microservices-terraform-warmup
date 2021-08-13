c87dd7203c76948481b8431ab2c31ef6d0668581
Alex Koufatzis <akoufa@hotmail.com>
2021-08-13 18:20:50 +0300

    Add deploy script to deploy k8s

---
+++ backend/kubernetes/deploy-k8s.sh
@@ -0,0 +1,15 @@
+kubectl apply -f namespace.yaml
+
+kubectl apply -f booksService/deployment.yaml
+kubectl apply -f booksService/service.yaml
+
+kubectl apply -f usersService/deployment.yaml
+kubectl apply -f usersService/service.yaml
+
+kubectl apply -f promotionsService/deployment.yaml
+kubectl apply -f promotionsService/service.yaml
+
+kubectl apply -f recommendationsService/deployment.yaml
+kubectl apply -f recommendationsService/service.yaml
+
+kubectl apply -f ingress.yaml
