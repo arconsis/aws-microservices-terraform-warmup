5a59f4a3eea2c9e0bc54cb87f5aee4952c021b07
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2022-03-18 09:01:24 +0100

    Temporarily disable default tags as tf produces errors

--- devops/aws/ecs_fargate/main.tf
+++ devops/aws/ecs_fargate/main.tf
@@ -12,9 +12,9 @@ provider "aws" {
   shared_credentials_files = ["$HOME/.aws/credentials"]
   profile                  = var.aws_profile
   region                   = var.aws_region
-  default_tags {
-    tags = var.default_tags
-  }
+#  default_tags {
+#    tags = var.default_tags
+#  }
 }
 
 module "networking" {
